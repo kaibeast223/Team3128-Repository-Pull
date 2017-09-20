9a4d8af11f8ea3c178d6f87bbcc506b260d4e8c2
kaibeast223 <kai.aspesi@gmail.com>
2017-09-19 20:33:57 -0700

    Add files via upload

---
+++ team3128githubcloner.sh
@@ -0,0 +1,19 @@
+Cd /Desktop/GITHUB_TEAM3128
+Git clone https://github.com/Team3128/2017-18-attendance-data.git
+Git clone https://github.com/Team3128/team3128.github.io.git
+Git clone https://github.com/Team3128/3128-robot-common.git
+Git clone https://github.com/Team3128/robot-Ferb.git
+Git clone https://github.com/Team3128/ScoutingScanner.git
+Git clone https://github.com/Team3128/MakeNarwhals.git
+Git clone https://github.com/Team3128/NarwhalVision.git
+Git clone https://github.com/Team3128/robot-kitbot.git
+Git clone https://github.com/Team3128/FRC-Test.git
+Git clone https://github.com/Team3128/info.git
+Git clone https://github.com/Team3128/robot-class-code.git
+Git clone https://github.com/Team3128/AttendanceScanner.git
+Git clone https://github.com/Team3128/cheesy-parts.git
+Git clone https://github.com/Team3128/bot.git
+Git clone https://github.com/Team3128/attendance-scripts.git
+Git clone https://github.com/Team3128/robot-data.git
+Git clone https://github.com/Team3128/Dashbored.git
+
